113a474a58f11e2722d33d0574efa104808c06f7
GSEvgenievich <Stepan.geras2006@yandex.ru>
2025-11-06 14:20:37 +0300

    0611

---
+++ Lection1106/AppDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lection1106
+{
+    internal class AppDbContext : DbContext
+    {
+        public DbSet<User > Users { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("Data Surce = app.db");
+        }
+    }
+}
---
+++ Lection1106/Program.cs
@@ -0,0 +1,76 @@
+﻿using BCrypt.Net;
+using Lection1106;
+using System.Security.Cryptography;
+using System.Text;
+
+Console.WriteLine("passwords");
+
+
+var login = "admin";
+var password = "123";
+using var context = new AppDbContext();
+var user = context.Users.FirstOrDefault(u => u.Login == login);
+
+if (user is null)
+{
+    Console.WriteLine("not found");
+    return;
+}
+
+if (user.LockedUntil.HasValue && user.LockedUntil >= DateTime.UtcNow)
+{
+    Console.WriteLine($"too early. wait {user.LockedUntil}");
+    return;
+}
+if (user.Password != password)
+{
+    user.FailedLoginAttempts++;
+    if (user.FailedLoginAttempts >= 3)
+        user.LockedUntil = DateTime.UtcNow.AddMinutes(1);
+
+    Console.WriteLine($"incorrect");
+    return;
+}
+user.LastAccess = DateTime.UtcNow;
+user.FailedLoginAttempts = 0;
+user.LockedUntil = null;
+context.SaveChanges();
+
+Console.WriteLine("welcome");
+
+static void ComputeHash()
+{
+    var solt = "123456";
+    var password = "qwerty" + solt;
+    byte[] bytes = Encoding.UTF8.GetBytes(password);
+
+    SHA384 algo = SHA384.Create();
+
+    var hashBytes = algo.ComputeHash(bytes);
+    var hash = Convert.ToBase64String(hashBytes); //base64
+    hash = Convert.ToHexString(hashBytes); // hex: 0-9A-F
+}
+
+static void ComputeBcryptHash()
+{
+    var password = "qwerty";
+    var hash = BCrypt.Net.BCrypt.EnhancedHashPassword(password);
+    Console.WriteLine(hash);
+
+    var input = "qwerty";
+    var isCorrect = BCrypt.Net.BCrypt.EnhancedVerify(input, hash);
+    Console.WriteLine(isCorrect);
+}
+
+static async Task InsertData()
+{
+    var users = new List<User>()
+{
+    new() {Login = "admin", Password = "qwerty"},
+    new() {Login = "manager", Password = "123"},
+    new() {Login = "customer", Password = "1"},
+};
+    var context = new AppDbContext();
+    context.Users.AddRange(users);
+    await context.SaveChangesAsync();
+}
